33ce6643b1fe9bc40b422b0b6a2ca7ee29455c87
you you <cyouyiun@gmail.com>
2021-02-06 02:34:14 +0000

    gooo

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ function getLocation(){
         const latitude  = position.coords.latitude;
         const longitude = position.coords.longitude;
 	  // do something
+    var map = new mapboxgl.Map({
+            style: 'mapbox://styles/mapbox/light-v10',
+            center: [longitude, latitude],
+            zoom: 15.5,
+            pitch: 45,
+            bearing: -17.6,
+            container: 'map',
+            antialias: true
+});
+
+var marker = new mapboxgl.Marker({
+            color: "#FF0000",
+            draggable: true
+}).setLngLat([longitude, latitude])
+.addTo(map);
     }
 
     function error(e) {
